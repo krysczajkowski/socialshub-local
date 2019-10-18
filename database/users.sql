312c120ae0fb78eab3a25d05156f435cb428f885
Krystian Czajkowski <czajkowski.krystian@gmail.com>
2019-10-18 12:55:46 +0200

    Updated database folder

--- database/users.sql
+++ database/users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 09, 2019 at 09:36 PM
+-- Generation Time: Oct 18, 2019 at 12:51 PM
 -- Server version: 10.1.33-MariaDB
 -- PHP Version: 7.2.6
 
@@ -45,12 +45,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `password`, `screenName`, `profileImage`, `profileCover`, `bio`, `validationCode`, `active`) VALUES
-(9, 'kulfon@gmail.com', '3fc0a7acf087f549ac2b266baf94b8b1', 'kredka', 'images/image9medusakredki.jpg', 'images/image9kot.jpg', 'Siema jestem kredka\r\nhihihihihi\r\nwbijaj na mojego google LOLOL: http://www.google.com', '0', 1),
-(10, 'czaja@gmail.com', '3fc0a7acf087f549ac2b266baf94b8b1', 'KrysCzajkowski', 'images/image10krolMacius1.jpg', 'images/image10delfinek.jpg', 'Elo mordo wbijaj na moje social media \r\nInst: krys.czajkowski\r\nSnap: nie_majuznazw\r\nFb: https://www.facebook.com/profile.php?id=100008701864345\r\nInstagramek: https://www.instagram.com/krys.czajkowski/', '0', 1),
-(14, 'krystian@gmail.com', '3fc0a7acf087f549ac2b266baf94b8b1', 'krystian', 'images/image14brzydkiKOt.jpg', 'images/image14bmw.jpg', 'Siema jestem KRYSTIAN!', '0', 1),
-(16, 'kicikici@gmai.com', '3fc0a7acf087f549ac2b266baf94b8b1', 'kicikici', 'images/defaultProfileImage.png', 'images/defaultCoverImage.png', '', '0', 1),
-(24, 'zdzichu@gmail.com', '3fc0a7acf087f549ac2b266baf94b8b1', 'zdzichu', 'images/defaultProfileImage.png', 'images/defaultCoverImage.png', '', 'bd60ccd4653cec34e26de1e47da54a4b', 0),
-(25, 'wujek@gmail.com', '3fc0a7acf087f549ac2b266baf94b8b1', 'wujek', 'images/defaultProfileImage.png', 'images/defaultCoverImage.png', '', '0', 1),
+(9, 'gracjan@gmail.com', '3fc0a7acf087f549ac2b266baf94b8b1', 'kredka', 'images/image9medusakredki.jpg', 'images/image9kot.jpg', 'Przykladowe Bio \r\n\r\nhttps://google.com\r\nhttp://facebook.com', '0', 1),
+(10, 'czajkowski.krystian@gmail.com', '3fc0a7acf087f549ac2b266baf94b8b1', 'KrysCzajkowski', 'images/image10krolMacius1.jpg', 'images/image10delfinek.jpg', 'xd', '0', 1),
+(14, 'krystian@gmail.com', '3fc0a7acf087f549ac2b266baf94b8b1', 'krystian', 'images/image14brzydkiKOt.jpg', 'images/image14bmw.jpg', 'Siema jestem KRYSTIAN!\r\na\r\na\r\na\r\na', '0', 1),
 (26, 'asdfasdff@gmail.com', '3fc0a7acf087f549ac2b266baf94b8b1', 'asdfasdf', 'images/defaultProfileImage.png', 'images/defaultCoverImage.png', '', '7c580605d38ed1a3e99a312ae37ce43a', 0);
 
 --
@@ -71,7 +68,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
