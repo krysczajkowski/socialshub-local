45720a7fe884c764a020e8ce8f2cdab132f377e5
Krystian Czajkowski <czajkowski.krystian@gmail.com>
2019-11-30 20:39:00 +0100

    Fixed account-data bug

--- account-data.php
+++ account-data.php
@@ -30,8 +30,13 @@ if(!$functions->loggedIn()) {
                         <a href="settings.php" class='text-dark h5 none-decoration'>Edit Profile</a>
                     </div>
                     <div class="col-12 my-3 pl-4">
+                        <a href="settings-links.php" class='text-dark h5 none-decoration'>My Links</a>
+                    </div>
+                    <?php if(!$functions->isUserFbUser($user->id)) {?>
+                    <div class="col-12 my-3 pl-4 ">
                         <a href="edit-password.php" class='text-dark h5 none-decoration'>Change Password</a>
                     </div>
+                    <?php } ?>
                     <div class="col-12 my-3 pl-4 border-left border-dark">
                         <a href="privacy_and_security.php" class='text-dark h5 none-decoration'>Privacy and Security</a>
                     </div>
