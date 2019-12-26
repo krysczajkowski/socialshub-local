43a0bbf5fd375167a538b3dcadc15011f1380102
Krystian Czajkowski <czajkowski.krystian@gmail.com>
2019-12-26 21:58:45 +0100

    Fixed search in signUp.php & signIn.php

--- signIn.php
+++ signIn.php
@@ -22,6 +22,12 @@
     <!-- reCaptcha invisible code  -->
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
        
+<script>
+    function onSubmit(token) {
+        document.getElementById("i-recaptcha").submit();
+    }
+</script>
+
     <?php include 'includes/head.php'; 
     
     if($functions->loggedIn()) {
@@ -110,7 +116,7 @@
                             }
                             ?>  
                         </div> 
-                        <a href="<?php echo $loginURL; ?>" class="text-center fb connect mt-2" style='width: 100%;' id='fb-index-button'>Continue with Facebook</a>                        
+                        <a href="<?php echo $loginURL; ?>" class="text-center fb connect mt-2 w-100" id='fb-index-button'>Continue with Facebook</a>                        
                         
                     </div>
 
@@ -145,9 +151,10 @@
     <?php }
     ?>
 
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src='js/search.js'></script>
         <script>
         $("#fb-index-button").on("click", function() {
             $("#fb-index-button").addClass("btn disabled");
