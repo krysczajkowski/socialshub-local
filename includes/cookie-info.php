c964ba291a9cfe70fc05f02dcd0a3ccb21513586
IgorKowalczyk <majonezexe@gmail.com>
2020-03-12 20:17:23 +0100

    Remove OK!

--- includes/cookie-info.php
+++ includes/cookie-info.php
@@ -4,7 +4,7 @@
         <div id="cookie_accept">
             <span style="font-size: 0.8rem;" class="pt-1">This site uses cookies to give you the best possible experience.<a href="privacy-policy.php" class="link"> Read More</a></span>
             <button type="button" class="close" data-dismiss="alert">
-                <span>OK!</span>
+                <span>x</span>
             </button>
         </div>
     </div>
